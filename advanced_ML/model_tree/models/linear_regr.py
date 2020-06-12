1377aeed50e30661029a670a2392160aff944fd3
trumiddd <ddzekounoff@trumidtech.com>
2020-06-12 07:27:35 -0400

    Update linear_regr.py

--- advanced_ML/model_tree/models/linear_regr.py
+++ advanced_ML/model_tree/models/linear_regr.py
@@ -3,15 +3,17 @@
  linear_regr.py  (author: Anson Wong / git: ankonzoid)
 
 """
-from sklearn.metrics import mean_squared_error
+from sklearn.metrics import mean_squared_error, mean_absolute_error
 import warnings
 warnings.filterwarnings(action="ignore", module="scipy", message="^internal gelsd")
 
 class linear_regr:
 
-    def __init__(self):
+    def __init__(self, criterion = "mse"):
         from sklearn.linear_model import LinearRegression
         self.model = LinearRegression()
+        self.criterion = criterion.lower()
+        self.loss_function = mean_absolute_error if self.criterion == "mae" else mean_squared_error
 
     def fit(self, X, y):
         self.model.fit(X, y)
@@ -20,5 +22,5 @@ class linear_regr:
         return self.model.predict(X)
 
     def loss(self, X, y, y_pred):
-        return mean_squared_error(y, y_pred)
+        return self.loss_function(y, y_pred)
 
